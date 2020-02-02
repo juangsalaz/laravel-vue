6ba4211be2324e281cae7956c71a104d44cc26a7
Juang Salaz Prabowo <juang.salaz.prabowo@gmail.com>
2020-02-03 01:37:13 +0700

    Update migration user table file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('title');
-            $table->string('tel_mobile');
-            $table->string('role');
+            $table->string('title')->nullable();
+            $table->string('tel_mobile')->nullable();
+            $table->string('role')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
